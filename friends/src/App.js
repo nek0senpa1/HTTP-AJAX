4839640adcad69d49d500aae9593090ae2e954de
amberm <nekosenpai@gmail.com>
2019-01-31 16:09:46 -0700

    got a button to add friends

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 //import ReactDom from 'react-dom';
 import axios from 'axios';
-//import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 
 import './App.css';
 import FriendsList from './FrindsList';
-//import DisplayFriends from './components/DisplayFriends';
+import FriendAdd from './components/FriendAdd';
 
 class App extends Component {
   constructor(){
@@ -31,15 +31,33 @@ class App extends Component {
     })
   }
 
+  addFriend = event => {
+    console.log('adding friendo here')
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>ALL THE FRIENDS!</h1>
+          <NavLink to="/add-form" className="nav-links">Add Friendo!</NavLink>
+
           
-          <FriendsList friends={this.state.friends} />
-          
+
+          <Route
+            exact path="/"
+            render ={props => <FriendsList friends={this.state.friends} />}
+          />
+
+          <Route
+            exact path="/add-form"
+            render={props => <FriendAdd {...props} addFriend={this.addFriend} />}
+          />
+
         </header>
+
+        
+
       </div>
     );
   }
